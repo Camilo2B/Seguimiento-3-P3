3f914df293d6d006259f0d4e8eac344129cb42eb
Camilo2B <camilo.ruizl@uqvirtual.edu.co>
2025-08-31 16:15:24 -0500

    final

--- Segumiento-3-P3/registro_vehiculos.exs
+++ Segumiento-3-P3/registro_vehiculos.exs
@@ -1,45 +1,58 @@
 # registro_vehiculos.exs
-
-defmodule RegistroVehiculos do
+defmodule RegistroVehiculosPeaje do
   def main do
-  Util.show_message("Registro de Vehículos")
-  placa = pedir_placa()
-  tipo_vehiculo = pedir_tipo_vehiculo()
-  peso_vehiculo = pedir_peso_vehiculo()
-  tarifa = calcular_tarifa(tipo_vehiculo, peso_vehiculo)
+    Util.show_message("Registro de Vehículos en Peaje")
+
+    placa = pedir_placa()
+    tipo  = pedir_tipo()
+    peso  = pedir_peso()
+
+    {placa_r, tipo_r, tarifa} = calcular_tarifa(placa, tipo, peso)
+
+    Util.show_message("Vehículo #{placa_r} (#{tipo_r}) debe pagar $#{tarifa}")
+    {placa_r, tipo_r, tarifa}
   end
+
+  # === Entradas ===
 
   def pedir_placa do
     placa = Util.input("Ingrese la placa del vehículo:", :string)
-    IO.puts "La placa del vehículo es: #{placa}"
+    IO.puts("Placa: #{placa}")
     placa
   end
 
-  def pedir_tipo_vehiculo do
-    tipo_vehiculo = Util.input("Ingrese el tipo de vehículo (carro, moto, camion):", :string)
-    IO.puts "El tipo de vehículo es: #{tipo_vehiculo}"
-    tipo_vehiculo
+  def pedir_tipo do
+    tipo = Util.input("Ingrese el tipo de vehículo (Carro, Moto, Camión):", :string)
+    tipo_limpio = tipo |> String.trim() |> String.downcase()
+
+    case tipo_limpio do
+      "carro"   -> "Carro"
+      "moto"    -> "Moto"
+      "camion"  -> "Camión"   # sin tilde
+      "camión"  -> "Camión"   # con tilde
+      _ ->
+        Util.show_message("Tipo inválido. Escriba Carro, Moto o Camión.")
+        pedir_tipo()
+    end
   end
 
-  def pedir_peso_vehiculo do
-    peso_vehiculo = Util.input("Ingrese el peso del vehículo en kg:", :float)
-    IO.puts "El peso del vehículo es: #{peso_vehiculo} toneladas"
-    peso_vehiculo
+  def pedir_peso do
+    peso = Util.input("Ingrese el peso del vehículo en toneladas:", :float)
+    IO.puts("Peso: #{peso} t")
+    peso
   end
 
-  def calcular_tarifa(tipo_vehiculo, peso_vehiculo) do
-    tarifa =
-      if tipo_vehiculo == "carro" do
-        10000
-        elif tipo_vehiculo == "moto" do
-          5000
-        else tipo_vehiculo == "camion"
-            20000 + (2000 * peso_vehiculo)
+  # === Cálculo ===
 
-        end
-      end
+  # Retorna una tupla con {placa, tipo, tarifa_final}
+  def calcular_tarifa(placa, "Carro", _peso), do: {placa, "Carro", 10_000}
+  def calcular_tarifa(placa, "Moto",  _peso), do: {placa, "Moto",   5_000}
+
+  def calcular_tarifa(placa, "Camión", peso) do
+    # Base 20,000 + 2,000 por tonelada adicional (peso puede ser real)
+    adicional = round(2_000 * peso)
+    {placa, "Camión", 20_000 + adicional}
   end
-
 end
 
-RegistroVehiculos.main()
+RegistroVehiculosPeaje.main()
